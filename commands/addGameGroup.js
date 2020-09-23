7faa28fd1131698a5c076454a0214169be04c017
Ifynth <ifynth@gmail.com>
2020-09-23 12:11:03 +0200

    Add command example strukture

---
+++ commands/addGameGroup.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'addGameGroup',
+    description: "Post a message, so people can react to it to get added to game group",
+    execute(message, args) {
+        console.log("call addGameGroup");
+    }
+}
--- main.js
+++ main.js
@@ -1,15 +1,37 @@
+const fs = require('fs')
 const Discord = require('discord.js')
 const { token } = require('./config.json')
 
 const client = new Discord.Client()
+client.commands = new Discord.Collection()
+
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  client.commands.set(command.name, command)
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+
+  if (msg.split(" ").find(word => word.toLowerCase().match("loona"))){
+
+    // const command = "addGameGroup"
+
+    // if(!client.commands.has(command)) return;
+
+    // try {
+    //   client.commands.get(command).execute(message, args);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
   }
 })
 
