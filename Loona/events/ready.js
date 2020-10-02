a6ca102a6330c10a73445aa148ccc38cd493c962
Ifynth <ifynth@gmail.com>
2020-10-02 19:02:16 +0200

    Add Create Loona Category on Ready

--- Loona/events/ready.js
+++ Loona/events/ready.js
@@ -1,6 +1,10 @@
+const { createCategory } = require('../helpers')
+
 module.exports = {
   name: 'ready',
   execute() {
     console.log(`${this.user.username} ist ready!`)
+
+    createCategory(this);
   },
 }
