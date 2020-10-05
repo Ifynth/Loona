07f3d3afcc6d7777de6fd7abeafe23bff131f917
Ifynth <ifynth@gmail.com>
2020-10-05 17:46:12 +0200

    Update Private Testing Messages

--- Loona/events/message.js
+++ Loona/events/message.js
@@ -1,14 +1,40 @@
+const { matchOr } = require('../helpers')
+
 module.exports = {
   name: 'message',
-  execute(msg) {
-    if (msg.split(' ').find((word) => word.toLowerCase().match('loona'))) {
-      // const command = "addGameGroup"
-      // if(!client.commands.has(command)) return;
-      // try {
-      //   client.commands.get(command).execute(message, args);
-      // } catch (error) {
-      //   console.error(error);
-      // }
+  execute(message) {
+    const { content } = message
+
+    // Direkt Message
+    if (message.channel.type === 'dm' && message.author.id !== this.user.id) {
+      const commands = []
+
+      for (const attr of this.loona.commands.values()) {
+        const { name, command } = attr
+        var connectTerms = true
+
+        for (const [searchTerm, terms] of Object.entries(command)) {
+          connectTerms = connectTerms && matchOr(content, terms)
+        }
+
+        if (connectTerms) commands.push(name)
+      }
+
+      console.log('Auszuführende Befehle: ', commands)
+
+      // call all commands
+      commands.map((name) => {
+        if (!this.loona.commands.has(name)) return
+
+        try {
+          this.loona.commands.get(name).execute(this, message)
+        } catch (error) {
+          console.error(error)
+        }
+      })
     }
+
+    // check if loona is mentioned (no need on direkt message)
+    if (!content.match(/Loona/i)) return
   },
 }
