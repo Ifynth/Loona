aa52c54f53e15d3e8a1d53817ecd3163576ca4c1
Ifynth <ifynth@gmail.com>
2020-10-16 13:47:41 +0200

    Add partials to react on not cached old messages

--- Loona/index.js
+++ Loona/index.js
@@ -2,12 +2,14 @@ const Discord = require('discord.js')
 const commands = require('./commands')
 const startEvents = require('./events')
 
-const client = new Discord.Client()
+const client = new Discord.Client({
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+})
 client.loona = { commands }
 
 module.exports = {
-    startEvents: () => startEvents(client),
-    login: (token) => {
-        client.login(token)
-    }
+  startEvents: () => startEvents(client),
+  login: (token) => {
+    client.login(token)
+  },
 }
