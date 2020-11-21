cb8ecb3834c44a58d6cdb309b0da4c11b037d019
Ifynth <ifynth@gmail.com>
2020-11-21 15:45:21 +0100

    Update message event strukture

--- Loona/events/message.js
+++ Loona/events/message.js
@@ -1,40 +1,42 @@
-const { matchOr } = require('../helpers')
+const { matchOr } = require("../helpers")
+
+const executeCommands = (client, message) => {
+  const { content } = message
+  const commands = []
+
+  for (const attr of client.loona.commands.values()) {
+    const { name, command } = attr
+    var connectTerms = true
+
+    for (const [searchTerm, terms] of Object.entries(command)) {
+      connectTerms = connectTerms && matchOr(content, terms)
+    }
+
+    if (connectTerms) commands.push(name)
+  }
+
+  console.log('Auszuführende Befehle: ', commands)
+
+  // call all commands
+  commands.map((name) => {
+    if (!client.loona.commands.has(name)) return
+
+    try {
+      client.loona.commands.get(name).execute(client, message)
+    } catch (error) {
+      console.error(error)
+    }
+  })
+}
 
 module.exports = {
   name: 'message',
   execute(message) {
     const { content } = message
 
-    // Direkt Message
-    if (message.channel.type === 'dm' && message.author.id !== this.user.id) {
-      const commands = []
+    // check if loona is mentioned (no need on direkt message) and don't react on self-post
+    if ((!content.match(/Loona/i) && message.channel.type !== 'dm') || message.author.id === this.user.id) return
 
-      for (const attr of this.loona.commands.values()) {
-        const { name, command } = attr
-        var connectTerms = true
-
-        for (const [searchTerm, terms] of Object.entries(command)) {
-          connectTerms = connectTerms && matchOr(content, terms)
-        }
-
-        if (connectTerms) commands.push(name)
-      }
-
-      console.log('Auszuführende Befehle: ', commands)
-
-      // call all commands
-      commands.map((name) => {
-        if (!this.loona.commands.has(name)) return
-
-        try {
-          this.loona.commands.get(name).execute(this, message)
-        } catch (error) {
-          console.error(error)
-        }
-      })
-    }
-
-    // check if loona is mentioned (no need on direkt message)
-    if (!content.match(/Loona/i)) return
+    executeCommands(this, message);
   },
 }
