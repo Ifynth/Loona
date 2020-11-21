d664badb301b4f2c41a95e0879344ccd020caf60
Ifynth <ifynth@gmail.com>
2020-11-21 15:34:53 +0100

    Update helpers

--- Loona/helpers/index.js
+++ Loona/helpers/index.js
@@ -1,3 +1,10 @@
-const matchOr = require('./matchOr')
+const matchOr = (text, terms, index = 0) => {
+    if (terms.length === index) return false
+    return (
+        !!text.match(new RegExp(terms[index], 'i')) || matchOr(text, terms, ++index)
+    )
+}
 
-module.exports = { matchOr }
+const getChannelName = (name) => name.trim().replace(" ", "-").toLocaleLowerCase()
+
+module.exports = { matchOr, getChannelName }
--- Loona/helpers/matchOr.js
+++
@@ -1,8 +0,0 @@
-const matchOr = (text, terms, index = 0) => {
-  if (terms.length === index) return false
-  return (
-    !!text.match(new RegExp(terms[index], 'i')) || matchOr(text, terms, ++index)
-  )
-}
-
-module.exports = matchOr
